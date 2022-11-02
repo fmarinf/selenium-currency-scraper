8786aa8e2839d47466231d70945e4b0ed2cc6de5
Fernando Marín Figueroa <102365693+fmarinf@users.noreply.github.com>
2022-11-02 17:17:06 +0000

    testing webdriver and browser

--- scraper.py
+++ scraper.py
@@ -1,1 +1,14 @@
-print("Hello World")
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+CURRENCY_CLP_URL = 'https://www.x-rates.com/table/?from=CLP&amount=1'
+
+chrome_options = Options()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome(options=chrome_options)
+
+driver.get(CURRENCY_CLP_URL)
+
+print('Page title: ', driver.title)
