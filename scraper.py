fef890d041107e97747c830f968eaf17d46e06d6
Fernando Marín Figueroa <102365693+fmarinf@users.noreply.github.com>
2022-11-03 00:49:49 +0000

    added counter to scraper

--- scraper.py
+++ scraper.py
@@ -1,7 +1,7 @@
+import json
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-import pandas as pd
 
 CURRENCY_CLP_URL = 'https://www.x-rates.com/table/?from=CLP&amount=1'
 
@@ -19,16 +19,16 @@ def get_values(driver):
   currencies = driver.find_elements(By.TAG_NAME,CURRENCY_TAG)
   return currencies
 
-def parse_currency(currency):
-  country_origin = currency.find_element(By.XPATH,'//*[@id="content"]/div[1]/div/div[1]/div[1]/table[1]/tbody/tr[1]/td[1]')
+def parse_currency(currency, x):
+  country_origin = currency.find_element(By.XPATH,f'//*[@id="content"]//div[1]//div//div[1]//div[1]//table[1]//tbody/tr[{x}]//td[1]')
   origin_currency = country_origin.text
-  
-  clp_equiv = currency.find_element(By.XPATH,'//*[@id="content"]/div[1]/div/div[1]/div[1]/table[1]/tbody/tr[1]/td[2]/a')
+    
+  clp_equiv = currency.find_element(By.XPATH,f'//*[@id="content"]/div[1]/div/div[1]/div[1]/table[1]/tbody/tr[{x}]/td[2]/a')
   clp_equivalent = float(clp_equiv.text)
-  
+    
   conv_url = clp_equiv.get_attribute('href')
-
-  clp_inv = currency.find_element(By.XPATH,'//*[@id="content"]/div[1]/div/div[1]/div[1]/table[1]/tbody/tr[1]/td[3]/a')
+  
+  clp_inv = currency.find_element(By.XPATH,f'//*[@id="content"]/div[1]/div/div[1]/div[1]/table[1]/tbody/tr[{x}]/td[3]/a')
   clp_inverted = float(clp_inv.text)
 
   return {
@@ -36,7 +36,7 @@ def parse_currency(currency):
     'clp_equivalent': clp_equivalent,
     'conversion_url': conv_url,
     'inverted_clp': clp_inverted
-  }
+    }
 
 if __name__ == "__main__":
   print('Creating driver...')
@@ -50,10 +50,12 @@ if __name__ == "__main__":
   # countryCurr, eqClp, invClp
 
   print('Parsing top 10 currencies: ')
-  currency_data = [parse_currency(currency) for currency in currencies[:10]]
-  print(currency_data)
- 
-  print('Saving as CSV..')
-  currencies_df = pd.DataFrame(currency_data)
-  print(currencies_df)
-  currencies_df.to_csv('currencies.csv', index=None)
+  currency_data = []
+  for currency, x in zip(currencies[:10] , range(1, 11)):
+    currency_data.append(parse_currency(currency, x))
+    x+=1
+    
+  # currency_data = [parse_currency(currency) for currency in currencies[:10]]
+
+  jsonStr = json.dumps(currency_data, indent=4)
+  print(jsonStr)
