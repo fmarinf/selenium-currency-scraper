df28732b9a43648bbb37b53c6beed259d65c1b8b
Fernando Marín Figueroa <102365693+fmarinf@users.noreply.github.com>
2022-11-03 01:33:22 +0000

    added AWS Lambda function

--- lambda_function.py
+++ lambda_function.py
@@ -1,3 +1,5 @@
+""" This file runs on AWS Lambda"""
+
 import json
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -6,12 +8,16 @@ from selenium.webdriver.common.by import By
 CURRENCY_CLP_URL = 'https://www.x-rates.com/table/?from=CLP&amount=1'
 
 def get_driver():
-  chrome_options = Options()
-  chrome_options.add_argument('--no-sandbox')
-  chrome_options.add_argument('--headless')
-  chrome_options.add_argument('--disable-dev-shm-usage')
-  driver = webdriver.Chrome(options=chrome_options)
-  return driver
+    options = Options()
+    options.binary_location = '/opt/headless-chromium'
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--single-process')
+    options.add_argument('--disable-dev-shm-usage')
+
+    driver = webdriver.Chrome('/opt/chromedriver',chrome_options=options)
+    
+    return driver
 
 def get_values(driver):
   CURRENCY_TAG = 'tr'
@@ -38,20 +44,30 @@ def parse_currency(currency, x):
     'inverted_clp': clp_inverted
     }
 
-if __name__ == "__main__":
-  print('Creating driver...')
-  driver = get_driver()
+def lambda_handler(event, context):
+    # Creating browser
+    driver = get_driver()
   
-  print('Fetching currency values...')
-  currencies = get_values(driver)
+    # Get values
+    currencies = get_values(driver)
   
-  print(f'Found {len(currencies)} values')
-
-  print('Parsing top 10 currencies: ')
-  currency_data = []
-  for currency, x in zip(currencies[:10] , range(1, 11)):
-    currency_data.append(parse_currency(currency, x))
-    x+=1
+    # Parsing top 10 currencies
+    currency_data = []
+    for currency, x in zip(currencies[:10] , range(1, 11)):
+        currency_data.append(parse_currency(currency, x))
+        x+=1
     
-  jsonStr = json.dumps(currency_data, indent=4)
-  print(jsonStr)
+    jsonStr = json.dumps(currency_data, indent=4)
+    
+    driver.close();
+    driver.quit();
+    
+    response = {
+        "statusCode": 200,
+        "body": jsonStr
+    }
+    
+    return response
+    
+if __name__ == "__main__":
+  lambda_handler(None, None)
